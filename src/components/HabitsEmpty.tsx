7c844d987aa97e22a44e10acd0a442dcee503590
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2023-01-29 10:31:45 -0300

    feat: HabitsEmpty

---
+++ src/components/HabitsEmpty.tsx
@@ -0,0 +1,9 @@
+import { Text } from "react-native";
+
+export function HabitsEmpty() {
+  return(
+    <Text className="text-zinc-400 text-base">
+      Você ainda não está monitorando nenhum hábito
+    </Text>
+  )
+}
