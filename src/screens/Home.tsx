2c10f705a1e5db6d3210660bfc66ed5db61487df
Leo Kazuyuki Nagatani <lknlkn15@hotmail.com>
2023-01-29 10:47:10 -0300

    fix: home screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,17 +1,19 @@
-import { useNavigation } from '@react-navigation/native'
-import dayjs from 'dayjs'
-import { useEffect, useState } from 'react'
-import { Alert, ScrollView, Text, View } from 'react-native'
-import { HabitDay, DAY_SIZE } from '../components/HabitDay'
+import { useCallback, useState } from 'react'
+import { Text, View, ScrollView, Alert } from 'react-native'
+import { useFocusEffect, useNavigation } from '@react-navigation/native'
+
+import { api } from '../lib/api'
+import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning'
+
 import { Header } from '../components/Header'
 import { Loading } from '../components/Loading'
-import { api } from '../lib/api'
+import { HabitDay, DAY_SIZE } from '../components/HabitDay'
+import dayjs from 'dayjs'
 
-import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning'
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
 const datesFromYearStart = generateDatesFromYearBeginning()
-const minimumSummaryDatesSizes = 18 * 7
-const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearStart.length
+const minimunSummaryDatesSizes = 18 * 5
+const amountOfDaysToFill = minimunSummaryDatesSizes - datesFromYearStart.length
 
 type SummaryProps = Array<{
   id: string
@@ -21,10 +23,10 @@ type SummaryProps = Array<{
 }>
 
 export function Home() {
-  const { navigate } = useNavigation()
-
   const [loading, setLoading] = useState(true)
   const [summary, setSummary] = useState<SummaryProps | null>(null)
+
+  const { navigate } = useNavigation()
 
   async function fetchData() {
     try {
@@ -38,9 +40,12 @@ export function Home() {
       setLoading(false)
     }
   }
-  useEffect(() => {
-    fetchData()
-  }, [])
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchData()
+    }, []),
+  )
 
   if (loading) {
     return <Loading />
@@ -49,10 +54,11 @@ export function Home() {
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
+
       <View className="flex-row mt-6 mb-2">
-        {weekDays.map((weekDay, index) => (
+        {weekDays.map((weekDay, i) => (
           <Text
-            key={`${weekDay}-${index}`}
+            key={`${weekDay}-${i}`}
             className="text-zinc-400 text-xl font-bold text-center mx-1"
             style={{ width: DAY_SIZE }}
           >
@@ -69,7 +75,7 @@ export function Home() {
           <View className="flex-row flex-wrap">
             {datesFromYearStart.map((date) => {
               const dayWithHabits = summary.find((day) => {
-                return dayjs(day.date).isSame(day.date, 'day')
+                return dayjs(date).isSame(day.date, 'day')
               })
 
               return (
@@ -77,17 +83,16 @@ export function Home() {
                   key={date.toISOString()}
                   date={date}
                   amountOfHabits={dayWithHabits?.amount}
-                  amountComplete={dayWithHabits?.completed}
+                  amountCompleted={dayWithHabits?.completed}
                   onPress={() =>
                     navigate('habit', { date: date.toISOString() })
                   }
                 />
               )
             })}
+
             {amountOfDaysToFill > 0 &&
-              Array.from({
-                length: amountOfDaysToFill,
-              }).map((_, index) => (
+              Array.from({ length: amountOfDaysToFill }).map((_, index) => (
                 <View
                   key={index}
                   className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
